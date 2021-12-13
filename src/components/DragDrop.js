684fc73a1fc99a09e4aea0a7ef4c159c390b912c
rajat <rajat2411gupta@gmail.com>
2021-12-13 19:11:43 +0530

    images

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -7,17 +7,17 @@ const PictureList = [
   {
     id: 1,
     url:
-      "https://yt3.ggpht.com/ytc/AAUvwnjOQiXUsXYMs8lwrd4litEEqXry1-atqJavJJ09=s900-c-k-c0x00ffffff-no-rj",
+      "/images/html.jpg",
   },
   {
     id: 2,
     url:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQExheo0sff_yQ/profile-displayphoto-shrink_200_200/0/1590072898568?e=1630540800&v=beta&t=_W-gWZewSBYQ-UAjpGvR8h_8Vvo202IHQQissbK2aKc",
+      "/images/css.jpg",
   },
   {
     id: 3,
     url:
-      "https://yt3.ggpht.com/pe57RF1GZibOWeZ9GwRWbjnLDCK2EEAeQ3u4iMAFNeaz-PN9uSsg1p2p32TZUedNnrUhKfoOuMM=s900-c-k-c0x00ffffff-no-rj",
+      "/images/js.png",
   },
 ];
 
@@ -36,6 +36,7 @@ function DragDrop() {
     const pictureList = PictureList.filter((picture) => id === picture.id);
     setBoard((board) => [...board, pictureList[0]]);
   };
+
   return (
     <>
       <div className="Pictures">
@@ -45,6 +46,7 @@ function DragDrop() {
       </div>
       <div className="Board" ref={drop}>
         {board.map((picture) => {
+          console.log(board)
           return <Picture url={picture.url} id={picture.id} />;
         })}
       </div>
